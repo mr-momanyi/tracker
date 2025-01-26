d96d72fbb7557e4ce23c84d68ccec540a0ea518a
Mr. Momanyi <justtinmomanyi@gmail.com>
2025-01-26 18:08:01 +0300

    Update ALLOWED_HOSTS for deployment on Render

--- tracker/settings.py
+++ tracker/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://tracker-1-07e7.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
